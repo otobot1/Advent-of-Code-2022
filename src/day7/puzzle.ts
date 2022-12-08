c2ddaa9791a22183aa84c515d1e5e7654a0c58a0
otobot1 <otobot1@gmail.com>
2022-12-07 22:26:41 -0700

    continue day 7

--- src/day7/puzzle.ts
+++ src/day7/puzzle.ts
@@ -10,7 +10,7 @@ type Directory = {
     subDirectories: string[];
 }
 
-type DirectoryList = {
+type DirectoryCollection = {
     [index: string]: Directory;
 }
 
@@ -24,8 +24,14 @@ export const main = () => {
 
 
 
+const cdPrefix = "$ cd ";
+const dirPrefix = "dir ";
+
+
+
+
 const puzzle1 = () => {
-    const inputPath = `${process.env.PROJECT_ROOT}/src/day7/test-input.txt`;
+    const inputPath = `${process.env.PROJECT_ROOT}/src/day7/input.txt`;
     if (!inputPath) throw "Invalid inputPath";
 
 
@@ -35,10 +41,115 @@ const puzzle1 = () => {
     const stringArray = fileContents.split(/\r?\n/);
 
 
-    const directoryList: DirectoryList = {};
-    let currentDirectory: Directory;
+    const directoryCollection = getDirectoryCollection(stringArray);
+
+
+    const overallSum = getOverallSum(directoryCollection);
+
+
+    console.log(overallSum);
+}
+
+
+
+
+const getDirectoryCollection = (stringArray: string[]): DirectoryCollection => {
+    const directoryCollection: DirectoryCollection = {
+        "/": {
+            name: "/",
+            parentDirectory: "",
+            directContentsSize: 0,
+            subDirectories: [],
+        }
+    };
+
+    let currentDirectory = directoryCollection["/"];
+
 
     for (const command of stringArray) {
+        if (command.startsWith(cdPrefix)) {
+            currentDirectory = handleCd(directoryCollection, currentDirectory, command);    //breaks here because code expects unique dir names :dead:
+            continue;
+        }
 
+
+        if (command.startsWith("$")) continue;   //handles "$ ls"
+
+
+        if (command.startsWith(dirPrefix)) {
+            const remainder = command.slice(dirPrefix.length);
+
+            currentDirectory.subDirectories.push(remainder);
+
+            continue;
+        }
+
+        
+        const size = Number(command.split(" ")[0]);
+
+        if (isNaN(size)) throw "size is NaN.";
+
+        currentDirectory.directContentsSize += size;
     }
+
+
+    return directoryCollection;
+}
+
+
+
+
+const handleCd = (directoryCollection: DirectoryCollection, currentDirectory: Directory, command: string): Directory => {
+    const remainder = command.slice(cdPrefix.length);
+
+    if (remainder === "..") return directoryCollection[currentDirectory.parentDirectory];
+
+
+    let newCurrentDirectory = directoryCollection[remainder];
+
+    if (!newCurrentDirectory) {
+        newCurrentDirectory = {
+            name: remainder,
+            parentDirectory: currentDirectory.name,
+            directContentsSize: 0,
+            subDirectories: [],
+        };
+
+        directoryCollection[remainder] = newCurrentDirectory;
+    }
+
+
+    return newCurrentDirectory;
+}
+
+
+
+
+const getOverallSum = (directoryCollection: DirectoryCollection): number => {
+    let overallSum = 0;
+
+
+    for (const [_primaryDirectoryName, primaryDirectory] of Object.entries(directoryCollection)) {
+        const directorySum = getSumRecursive(directoryCollection, primaryDirectory);
+
+        if (directorySum <= 100000) overallSum += directorySum;
+    }
+
+
+    return overallSum;
+}
+
+
+
+
+const getSumRecursive = (directoryCollection: DirectoryCollection, directory: Directory): number => {
+    let sum = directory.directContentsSize;
+
+
+    for (const subDirectoryName of directory.subDirectories) {
+        sum += getSumRecursive(directoryCollection, directoryCollection[subDirectoryName]);
+    }
+
+
+    return sum;
 }

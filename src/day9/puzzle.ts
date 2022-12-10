c402f875cfa55eb39b1802f3237951343ff6f331
otobot1 <otobot1@gmail.com>
2022-12-10 15:20:17 -0700

    completed day 9

--- src/day9/puzzle.ts
+++ src/day9/puzzle.ts
@@ -4,7 +4,7 @@ import fs from "fs";
 
 
 export const main = () => {
-    puzzle1();
+    puzzle2();
 }
 
 
@@ -21,7 +21,7 @@ const puzzle1 = () => {
     const stringArray = fileContents.split(/\r?\n/);
 
 
-    const count = getCount(stringArray);
+    const count = getCount(stringArray, 2);
 
 
     console.log(count);
@@ -30,19 +30,37 @@ const puzzle1 = () => {
 
 
 
-const getCount = (stringArray: string[]) => {
-    const ARRAY_LENGTH = 1000;
-    const startingPosition = Math.floor(ARRAY_LENGTH / 2);
+const puzzle2 = () => {
+    const inputPath = `${process.env.PROJECT_ROOT}/src/day9/input.txt`;
+    if (!inputPath) throw "Invalid inputPath";
+
+
+    const fileContents = fs.readFileSync(inputPath, "utf8");
+
+
+    const stringArray = fileContents.split(/\r?\n/);
+
+
+    const count = getCount(stringArray, 10);
+
+
+    console.log(count);
+}
+
+
+
+
+const getCount = (stringArray: string[], knotCount: number) => {
+    const GRID_LENGTH = 5000;
+    const startingPosition = Math.floor(GRID_LENGTH / 2);
 
 
     //outer array elements are rows, inner array elements are columns. origin is in top left of grid
-    const positions: number[][] = Array(ARRAY_LENGTH).fill(null).map(() => Array(ARRAY_LENGTH).fill(0));
+    const grid: number[][] = Array(GRID_LENGTH).fill(null).map(() => Array(GRID_LENGTH).fill(0));
 
+    const positions: [number, number][] = Array(knotCount).fill(null).map(() => [startingPosition, startingPosition]);
 
-    const headPosition: [number, number] = [startingPosition, startingPosition];        //[X, Y]
-    const tailPosition: [number, number] = [startingPosition, startingPosition];        //[X, Y]
-
-    positions[startingPosition][startingPosition] = 1;
+    grid[startingPosition][startingPosition] = 1;
 
 
     for (const string of stringArray) {
@@ -52,17 +70,17 @@ const getCount = (stringArray: string[]) => {
         if (isNaN(moveLength)) throw "moveLength is NaN.";
 
 
-        if (direction === "R") moveRight(positions, headPosition, tailPosition, moveLength);
-        else if (direction === "L") moveLeft(positions, headPosition, tailPosition, moveLength);
-        else if (direction === "U") moveUp(positions, headPosition, tailPosition, moveLength);
-        else if (direction === "D") moveDown(positions, headPosition, tailPosition, moveLength);
+        if (direction === "R") moveRight(grid, positions, knotCount, moveLength);
+        else if (direction === "L") moveLeft(grid, positions, knotCount, moveLength);
+        else if (direction === "U") moveUp(grid, positions, knotCount, moveLength);
+        else if (direction === "D") moveDown(grid, positions, knotCount, moveLength);
         else throw "Invalid direction.";
     }
 
 
     let count = 0;
 
-    for (const row of positions) {
+    for (const row of grid) {
         for (const position of row) {
             if (position === 1) count++;
         }
@@ -75,113 +93,184 @@ const getCount = (stringArray: string[]) => {
 
 
 
-const moveRight = (positions: number[][], headPosition: [number, number], tailPosition: [number, number], moveLength: number): void => {
+const moveRight = (grid: number[][], positions: [number, number][], knotCount: number, moveLength: number): void => {
     while (moveLength > 0) {
         moveLength--;
 
 
-        headPosition[0] = headPosition[0] + 1;
+        positions[0][0] = positions[0][0] + 1;
 
 
-        const tailX = tailPosition[0];
-        const tailY = tailPosition[1];
-        const headX = headPosition[0];
-        const headY = headPosition[1];
+        for (let leaderIndex = 0; leaderIndex < knotCount - 1; leaderIndex++) {
+            const followerIndex = leaderIndex + 1;
+
+            const leader = positions[leaderIndex];
+            const follower = positions[followerIndex];
+
+            const leaderX = leader[0];
+            const leaderY = leader[1];
+            const followerX = follower[0];
+            const followerY = follower[1];
 
 
-        if (
-            (tailX === headX || tailX === headX + 1 || tailX === headX - 1) &&
-            (tailY === headY || tailY === headY + 1 || tailY === headY - 1)
-        ) continue;     //still touching. tail doesn't move.
-
-        tailPosition[0] = headX - 1;
-        tailPosition[1] = headY;
+            if (
+                (leaderX === followerX || leaderX === followerX + 1 || leaderX === followerX - 1) &&
+                (leaderY === followerY || leaderY === followerY + 1 || leaderY === followerY - 1)
+            ) break;        //still touching. follower doesn't move.
 
 
-        positions[tailPosition[0]][tailPosition[1]] = 1;
+            handleFollower(follower, leaderX, leaderY, followerX, followerY);
+
+
+            if (followerIndex === knotCount - 1) grid[follower[0]][follower[1]] = 1;
+        }
     }
 }
 
 
-const moveLeft = (positions: number[][], headPosition: [number, number], tailPosition: [number, number], moveLength: number): void => {
+const moveLeft = (grid: number[][], positions: [number, number][], knotCount: number, moveLength: number): void => {
     while (moveLength > 0) {
         moveLength--;
 
 
-        headPosition[0] = headPosition[0] - 1;
+        positions[0][0] = positions[0][0] - 1;
 
 
-        const tailX = tailPosition[0];
-        const tailY = tailPosition[1];
-        const headX = headPosition[0];
-        const headY = headPosition[1];
+        for (let leaderIndex = 0; leaderIndex < knotCount - 1; leaderIndex++) {
+            const followerIndex = leaderIndex + 1;
+
+            const leader = positions[leaderIndex];
+            const follower = positions[followerIndex];
+
+            const leaderX = leader[0];
+            const leaderY = leader[1];
+            const followerX = follower[0];
+            const followerY = follower[1];
 
 
-        if (
-            (tailX === headX || tailX === headX + 1 || tailX === headX - 1) &&
-            (tailY === headY || tailY === headY + 1 || tailY === headY - 1)
-        ) continue;     //still touching. tail doesn't move.
-
-        tailPosition[0] = headX + 1;
-        tailPosition[1] = headY;
+            if (
+                (leaderX === followerX || leaderX === followerX + 1 || leaderX === followerX - 1) &&
+                (leaderY === followerY || leaderY === followerY + 1 || leaderY === followerY - 1)
+            ) break;        //still touching. follower doesn't move.
 
 
-        positions[tailPosition[0]][tailPosition[1]] = 1;
+            handleFollower(follower, leaderX, leaderY, followerX, followerY);
+
+
+            if (followerIndex === knotCount - 1) grid[follower[0]][follower[1]] = 1;
+        }
     }
 }
 
 
-const moveUp = (positions: number[][], headPosition: [number, number], tailPosition: [number, number], moveLength: number): void => {
+const moveUp = (grid: number[][], positions: [number, number][], knotCount: number, moveLength: number): void => {
     while (moveLength > 0) {
         moveLength--;
 
 
-        headPosition[1] = headPosition[1] - 1;
+        positions[0][1] = positions[0][1] - 1;
 
 
-        const tailX = tailPosition[0];
-        const tailY = tailPosition[1];
-        const headX = headPosition[0];
-        const headY = headPosition[1];
+        for (let leaderIndex = 0; leaderIndex < knotCount - 1; leaderIndex++) {
+            const followerIndex = leaderIndex + 1;
+
+            const leader = positions[leaderIndex];
+            const follower = positions[followerIndex];
+
+            const leaderX = leader[0];
+            const leaderY = leader[1];
+            const followerX = follower[0];
+            const followerY = follower[1];
 
 
-        if (
-            (tailX === headX || tailX === headX + 1 || tailX === headX - 1) &&
-            (tailY === headY || tailY === headY + 1 || tailY === headY - 1)
-        ) continue;     //still touching. tail doesn't move.
-
-        tailPosition[0] = headX;
-        tailPosition[1] = headY + 1;
+            if (
+                (leaderX === followerX || leaderX === followerX + 1 || leaderX === followerX - 1) &&
+                (leaderY === followerY || leaderY === followerY + 1 || leaderY === followerY - 1)
+            ) break;        //still touching. follower doesn't move.
 
 
-        positions[tailPosition[0]][tailPosition[1]] = 1;
+            handleFollower(follower, leaderX, leaderY, followerX, followerY);
+
+
+            if (followerIndex === knotCount - 1) grid[follower[0]][follower[1]] = 1;
+        }
     }
 }
 
 
-const moveDown = (positions: number[][], headPosition: [number, number], tailPosition: [number, number], moveLength: number): void => {
+const moveDown = (grid: number[][], positions: [number, number][], knotCount: number, moveLength: number): void => {
     while (moveLength > 0) {
         moveLength--;
 
 
-        headPosition[1] = headPosition[1] + 1;
+        positions[0][1] = positions[0][1] + 1;
 
 
-        const tailX = tailPosition[0];
-        const tailY = tailPosition[1];
-        const headX = headPosition[0];
-        const headY = headPosition[1];
+        for (let leaderIndex = 0; leaderIndex < knotCount - 1; leaderIndex++) {
+            const followerIndex = leaderIndex + 1;
+
+            const leader = positions[leaderIndex];
+            const follower = positions[followerIndex];
+
+            const leaderX = leader[0];
+            const leaderY = leader[1];
+            const followerX = follower[0];
+            const followerY = follower[1];
 
 
-        if (
-            (tailX === headX || tailX === headX + 1 || tailX === headX - 1) &&
-            (tailY === headY || tailY === headY + 1 || tailY === headY - 1)
-        ) continue;     //still touching. tail doesn't move.
-
-        tailPosition[0] = headX;
-        tailPosition[1] = headY - 1;
+            if (
+                (leaderX === followerX || leaderX === followerX + 1 || leaderX === followerX - 1) &&
+                (leaderY === followerY || leaderY === followerY + 1 || leaderY === followerY - 1)
+            ) break;        //still touching. follower doesn't move.
 
 
-        positions[tailPosition[0]][tailPosition[1]] = 1;
+            handleFollower(follower, leaderX, leaderY, followerX, followerY);
+
+
+            if (followerIndex === knotCount - 1) grid[follower[0]][follower[1]] = 1;
+        }
     }
+}
+
+
+
+
+const handleFollower = (follower: [number, number], leaderX: number, leaderY: number, followerX: number, followerY: number): void => {
+    if (leaderX === followerX + 2) {
+        follower[0] = followerX + 1;
+
+        if (leaderY === followerY) return;
+        else if (leaderY === followerY + 1 || leaderY === followerY - 1) follower[1] = leaderY;
+        else if (leaderY === followerY + 2) follower[1] = followerY + 1;
+        else if (leaderY === followerY - 2) follower[1] = followerY - 1;
+        else throw "Invalid difference."
+    }
+    else if (leaderX === followerX - 2) {
+        follower[0] = followerX - 1;
+
+        if (leaderY === followerY) return;
+        else if (leaderY === followerY + 1 || leaderY === followerY - 1) follower[1] = leaderY;
+        else if (leaderY === followerY + 2) follower[1] = followerY + 1;
+        else if (leaderY === followerY - 2) follower[1] = followerY - 1;
+        else throw "Invalid difference."
+    }
+    else if (leaderY === followerY + 2) {
+        follower[1] = followerY + 1;
+
+        if (leaderX === followerX) return;
+        else if (leaderX === followerX + 1 || leaderX === followerX - 1) follower[0] = leaderX;
+        else if (leaderX === followerX + 2) follower[0] = followerX + 1;
+        else if (leaderX === followerX - 2) follower[0] = followerX - 1;
+        else throw "Invalid difference."
+    }
+    else if (leaderY === followerY - 2) {
+        follower[1] = followerY - 1;
+
+        if (leaderX === followerX) return;
+        else if (leaderX === followerX + 1 || leaderX === followerX - 1) follower[0] = leaderX;
+        else if (leaderX === followerX + 2) follower[0] = followerX + 1;
+        else if (leaderX === followerX - 2) follower[0] = followerX - 1;
+        else throw "Invalid difference."
+    }
+    else throw "Invalid difference.";
 }

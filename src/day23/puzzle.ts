93bc65df19707b5850fcaacc60d6597c2b5ed415
otobot1 <otobot1@gmail.com>
2022-12-12 09:02:39 -0700

    create files for remaining days

---
+++ src/day23/puzzle.ts
@@ -0,0 +1,22 @@
+import fs from "fs";
+
+
+
+
+export const main = () => {
+    puzzle1();
+}
+
+
+
+
+const puzzle1 = () => {
+    const inputPath = `${process.env.PROJECT_ROOT}/src/day23/test-input.txt`;
+    if (!inputPath) throw "Invalid inputPath";
+
+
+    const fileContents = fs.readFileSync(inputPath, "utf8");
+
+
+    const stringArray = fileContents.split(/\r?\n/);
+}

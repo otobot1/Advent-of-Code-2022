b4f6ec65507c97853e444b009b3e067468f94e2f
otobot1 <otobot1@gmail.com>
2022-12-20 15:07:09 -0700

    prep files for remaining puzzles

--- src/day17/puzzle.ts
+++ src/day17/puzzle.ts
@@ -3,15 +3,35 @@ import fs from "fs";
 
 
 
+const isTest = false;
+const isPartTwo = true;
+
+
+
+
 export const main = () => {
-    puzzle1();
+    isPartTwo ? puzzle2() : puzzle1();
 }
 
 
 
 
 const puzzle1 = () => {
-    const inputPath = `${process.env.PROJECT_ROOT}/src/day17/test-input.txt`;
+    const inputPath = `${process.env.PROJECT_ROOT}/src/day17/${isTest ? "test-" : ""}input.txt`;
+    if (!inputPath) throw "Invalid inputPath";
+
+
+    const fileContents = fs.readFileSync(inputPath, "utf8");
+
+
+    const stringArray = fileContents.split(/\r?\n/);
+}
+
+
+
+
+const puzzle2 = () => {
+    const inputPath = `${process.env.PROJECT_ROOT}/src/day17/${isTest ? "test-" : ""}input.txt`;
     if (!inputPath) throw "Invalid inputPath";
 
 
